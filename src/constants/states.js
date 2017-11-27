14aafa1a8ba2c5323d0c4abc572e9a10a42f2cd1
ChrisPlease <chris@chrisplease.me>
2017-11-26 19:21:32 -0700

    add new constants

---
+++ src/constants/states.js
@@ -0,0 +1,17 @@
+const initialState = {
+  weight: '',
+  age: '',
+  measurements: {
+    first: 0,
+    second: 0,
+    third: 0
+  },
+  bodyDensity: 0,
+  bodyFat: 0,
+  leanBodyMass: 0,
+  weightInFat: 0
+};
+
+export {
+  initialState
+}
